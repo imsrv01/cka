38243e8f436fa0b832d56d1132da21ee46ba4380
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-08-10 12:26:21 -0500

    Update generateCerts.sh

--- K8sHardway/generateCerts.sh
+++ K8sHardway/generateCerts.sh
@@ -43,7 +43,7 @@ cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
 
 # Admin client cert
-# Input --. admin-csr.json, CN=admin, O=systems-masters
+# Input --. admin-csr.json, CN=admin, O=systems:masters
 # Output --> admin.pem, admin-key.pem
 
 
@@ -76,3 +76,44 @@ cfssl gencert \
   admin-csr.json | cfssljson -bare admin
 
 }
+
+
+# Worker client cert
+# Input --. worker-0-csr.json, CN=system:node:worker-0, O=systems:node, -hostname=worker-0,<internalIP>,<externalIP>
+# Output --> worker-0.pem, worker-0-key.pem
+
+
+for instance in worker-0 worker-1 worker-2; do
+cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=$(gcloud compute instances describe ${instance} \
+  --format 'value(networkInterfaces[0].accessConfigs[0].natIP)')
+
+INTERNAL_IP=$(gcloud compute instances describe ${instance} \
+  --format 'value(networkInterfaces[0].networkIP)')
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${instance},${EXTERNAL_IP},${INTERNAL_IP} \
+  -profile=kubernetes \
+  ${instance}-csr.json | cfssljson -bare ${instance}
+done
