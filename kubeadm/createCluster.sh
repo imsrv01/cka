0831660139d7795c5be894b27b36008622cb8483
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-08-10 23:09:29 -0500

    Update createCluster.sh

--- kubeadm/createCluster.sh
+++ kubeadm/createCluster.sh
@@ -1,5 +1,26 @@
+gcloud config set compute/region us-central1
 
-  for i in 0; do
+gcloud config set compute/zone us-central1-a
+
+gcloud compute networks create kubeadm --subnet-mode custom
+
+gcloud compute networks subnets create kubeadm \
+  --network kubeadm \
+  --range 10.240.0.0/24
+  
+gcloud compute firewall-rules create kubeadm-allow-internal \
+  --allow tcp,udp,icmp \
+  --network kubeadm \
+  --source-ranges 10.240.0.0/24,10.244.0.0/16
+
+gcloud compute firewall-rules create kubeadm-allow-external \
+  --allow tcp:22,tcp:6443,icmp \
+  --network kubeadm \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules list --filter="network:kubeadm"
+
+for i in 0; do
   gcloud compute instances create master-${i} \
     --async \
     --boot-disk-size 200GB \
@@ -7,19 +28,22 @@
     --image-family ubuntu-1804-lts \
     --image-project ubuntu-os-cloud \
     --machine-type n1-standard-1 \
-    --private-network-ip 10.240.0.3${i} \
+    --private-network-ip 10.240.0.2${i} \
     --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
-    --subnet kubernetes \
-    --tags kubeadm,controller
+    --subnet kubeadm \
+    --tags kubeadm,master
 done
 
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+gcloud compute ssh master-0
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
