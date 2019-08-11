853e56fc150adf45fb8878859fa0c3f1352ff2ae
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-08-11 00:19:56 -0500

    Update createCluster.sh

--- kubeadm/createCluster.sh
+++ kubeadm/createCluster.sh
@@ -70,3 +70,13 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml
+
+kubectl get nodes
+
