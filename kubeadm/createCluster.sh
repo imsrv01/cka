c1f99a9f067cab7156cfd8c879360514e56020ad
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-08-10 22:45:31 -0500

    Update createCluster.sh

--- kubeadm/createCluster.sh
+++ kubeadm/createCluster.sh
@@ -1,4 +1,18 @@
 
+  for i in 0; do
+  gcloud compute instances create master-${i} \
+    --async \
+    --boot-disk-size 200GB \
+    --can-ip-forward \
+    --image-family ubuntu-1804-lts \
+    --image-project ubuntu-os-cloud \
+    --machine-type n1-standard-1 \
+    --private-network-ip 10.240.0.3${i} \
+    --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+    --subnet kubernetes \
+    --tags kubeadm,controller
+done
+
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
